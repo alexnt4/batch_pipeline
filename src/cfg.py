8b5707b25dee306b88dcf2225892d36b40baa0b0
Alex <kend1123ue@gmail.com>
2025-04-26 21:11:45 -0500

    add: new structure

--- src/cfg.py
+++
@@ -1,9 +0,0 @@
-from decouple import Config, RepositoryIni
-
-# Especifica la ruta a tu archivo .ini
-config = Config(RepositoryIni("settings.ini"))
-
-CLIENT_ID = config("CLIENT_ID")
-CLIENT_SECRET = config("CLIENTE_SECRET")
-SPOTIPY_REDIRECT_URL = config("SPOTIPY_REDIRECT_URI")
-DB_CONNSTR = config("DB_CONNSTR")
